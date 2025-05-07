0579f0c3c32bd551756c90760072451ba0c53571
ankitt-singh <ankitt.kr.singhh@gmail.com>
2025-05-07 09:56:30 +0530

    new changes

---
+++ firebase_config.js
@@ -0,0 +1,12 @@
+// // firebase-config.js
+// const firebaseConfig = {
+//     apiKey: "AIzaSyBGMT3v_6fopeh78Ze44AJIPPBM-4FczFM",
+//     authDomain: "codecracker-7e97a.firebaseapp.com",
+//     projectId: "codecracker-7e97a",
+//     storageBucket: "codecracker-7e97a.appspot.com",
+//     messagingSenderId: "747903395548",
+//     appId: "1:747903395548:web:5e6fb1dd81f751927cf619",
+//     measurementId: "G-PZXD0YT8ND"
+//   };
+  
+//   export default firebaseConfig;
